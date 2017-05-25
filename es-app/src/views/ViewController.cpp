4585d07191a5d6646e3666728cd5b4fdb129d4db
hex007 <newton3143@gmail.com>
2017-05-24 21:33:06 -0700

    resolve git problem

--- es-app/src/views/ViewController.cpp
+++ es-app/src/views/ViewController.cpp
@@ -314,10 +314,7 @@ std::shared_ptr<IGameListView> ViewController::getGameListView(SystemData* syste
 	{
 		case VIDEO:
 			view = std::shared_ptr<IGameListView>(new VideoGameListView(mWindow, system->getRootFolder()));
-<<<<<<< HEAD
 			setPowerSaver(false);
-=======
->>>>>>> 3bfb78b110ef57cb1c5e3e3addc072b75febcf4d
 			break;
 		case DETAILED:
 			view = std::shared_ptr<IGameListView>(new DetailedGameListView(mWindow, system->getRootFolder()));
